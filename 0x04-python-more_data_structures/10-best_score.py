edb68cf3648884049b6fa22fccda4bad2ab39f85
Bervelyn Sawyerr-Markwei <bervsawmar@gmail.com>
2023-01-13 23:21:51 +0000

    Data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    new_1 = []
-    for x in my_list:
-        if x == search:
-            new_1.append(replace)
-        else:
-            new_1.append(x)
-    return new_1
+def best_score(my_dict):
+    if my_dict and len(my_dict):
+        max = list(my_dict.keys())[0]
+        for key in my_dict:
+            if my_dict[key] > my_dict[max]:
+                max = key
+        return max
+    return None
